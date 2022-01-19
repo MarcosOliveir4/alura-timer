d3c9103807fb755d76f721566575fa9c9f583a6e
MarcosOliveir4 <marcosassis071@gmail.com>
2022-01-18 21:29:09 -0300

    :sparkles: primeira janela

---
+++ main.js
@@ -0,0 +1,10 @@
+const { app, BrowserWindow } = require("electron");
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({
+    with: 600,
+    height: 400,
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+});
